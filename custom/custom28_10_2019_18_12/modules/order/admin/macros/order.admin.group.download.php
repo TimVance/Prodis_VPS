3c31b5a6c54aba2dee661fa615f811f7ceecd2f4
TimVance <2DngxvshA6GUGcQ>
2020-08-14 11:02:04 +0300

    Добавил выбор типа документа от трекера

--- custom/custom28_10_2019_18_12/modules/order/admin/macros/order.admin.group.download.php
+++ custom/custom28_10_2019_18_12/modules/order/admin/macros/order.admin.group.download.php
@@ -52,10 +52,16 @@ class Order_admin_group_download extends Diafan
         $ids = $this->diafan->filter($_POST["ids"], "integer");
 
         if (count($ids) == 1) {
-            $files = $this->getFile($ids[0]);
+            $params = $this->getOrderParamElement($ids[0]);
+
+            // Вид документа
+            if ($params[19]["value"] == 9) $type_docs = 5;
+            else $type_docs = 6;
+
+
+            $files = $this->getFile($ids[0], $type_docs);
             $urls = array();
             foreach ($files as $number_file => $file) {
-                $params = $this->getOrderParamElement($ids[0]);
                 require $_SERVER["DOCUMENT_ROOT"] . '/custom/custom28_10_2019_18_12/plugins/vendor/autoload.php';
                 $phpWord = new PhpOffice\PhpWord\PhpWord();
                 $doc     = new PhpOffice\PhpWord\TemplateProcessor('https://' . $_SERVER['HTTP_HOST'] . '/attachments/get/' . $file["id"] . '/' . $file["name"]);
@@ -228,14 +234,14 @@ class Order_admin_group_download extends Diafan
     }
 
     // Получение файла
-    private function getFile($id)
+    private function getFile($id, $type = 5)
     {
         return DB::query_fetch_all("
                 SELECT file.id, file.name FROM {shop_order} AS orders
                 RIGHT JOIN {shop_order_goods} AS goods ON goods.order_id=orders.id
                 RIGHT JOIN {attachments} AS file ON goods.good_id=file.element_id
                 WHERE orders.id='%d' AND file.param_id='%d'
-            ", $id, 5);
+            ", $id, $type);
     }
 
     // Получение параметров списка товаров
